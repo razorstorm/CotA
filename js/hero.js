139f88ed27796b7d2ea980a11b913c3edabef3d2
Jason Jiang <jajiang@jajiang-mn1.linkedin.biz>
2013-11-07 14:51:12 -0800

    First commit: including basic scss defining the panes and one half made class

---
+++ js/hero.js
@@ -0,0 +1,12 @@
+function Hero()
+{
+    this.level=1;
+    this.setStats();
+    this.mp=this.mpMax;
+    this.damageLow = 20;
+    this.damageHeight = 34;
+    this.gold=800;
+    this.exp=0;
+    this.saveState();
+    this.inventory = Inventory();
+}
